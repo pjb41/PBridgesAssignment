1895d686de24eb35023d4d634aa9410fc7b90079
pjb41 <pjb41@hotmail.com>
2021-04-27 17:20:59 +0100

    Update Assign1.py

--- Assign1.py
+++ Assign1.py
@@ -8,8 +8,9 @@ import rasterio as rio
 from rasterio.plot import show
 import fiona as f
 import numpy as np
+import matplotlib.lines as mlines
 
-
+plt.ion()
 
 def generate_handles(labels, colors, edge='k', alpha=1):
     lc = len(colors)
@@ -28,19 +29,41 @@ def scale_bar(ax, location=(0.92, 0.95)):
     sbx = x0 + (x1 - x0) * location[0]
     sby = y0 + (y1 - y0) * location[1]
 
-    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=9, transform=tmc)
-    plt.plot([sbx, sbx - 5000], [sby, sby], color='k', linewidth=6, transform=tmc)
-    plt.plot([sbx-5000, sbx - 10000], [sby, sby], color='w', linewidth=6, transform=tmc)
+    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
+    plt.plot([sbx, sbx - 5000], [sby, sby], color='k', linewidth=4, transform=tmc)
+    plt.plot([sbx-5000, sbx - 10000], [sby, sby], color='w', linewidth=4, transform=tmc)
 
+    plt.text(sbx, sby-4500, '10km', transform=tmc, fontsize=8)
+    plt.text(sbx-12500, sby-4500, '5km', transform=tmc, fontsize=8)
+    plt.text(sbx-24500, sby-4500, '0km', transform=tmc, fontsize=8)
 
+outline = gpd.read_file('Data/outline_kent.shp')
 seadef = gpd.read_file('Data/seadef_sel.shp')
 dtm = gpd.read_file('Data/Raster_tr35_dt3.shp')
-myCRS = ccrs.UTM(29)
-myFig, ax = plt.subplots(1, 1, figsize=(12,12), subplot_kw=dict(projection=myCRS))
 
+myFig = plt.figure(figsize=(12, 12))
 
+myCRS = ccrs.UTM(31)
 
 ax = plt.axes(projection=ccrs.Mercator())
+
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='gray')
+xmin, ymin, xmax, ymax = outline.total_bounds
+ax.add_feature(outline_feature)
+
+inset_feature = ShapelyFeature(seadef['geometry'], myCRS, edgecolor='k', facecolor='CornflowerBlue')
+xmin, ymin, xmax, ymax = seadef.total_bounds
+ax.add_feature(inset_feature)
+
+top_feature = ShapelyFeature(dtm['geometry'], myCRS, edgecolor='k', facecolor='w')
+xmin, ymin, xmax, ymax = dtm.total_bounds
+ax.add_feature(top_feature)
+
+
+
+ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)
+
+myFig.savefig('assign1.png', bbox_inches= 'tight', dpi=300)
 
 
 
