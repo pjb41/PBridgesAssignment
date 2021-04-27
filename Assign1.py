db96d38275c8719d16f14360abf10f2fc0c6eadb
pjb41 <pjb41@hotmail.com>
2021-04-27 16:10:36 +0100

    Update Assign1.py

--- Assign1.py
+++ Assign1.py
@@ -9,6 +9,8 @@ from rasterio.plot import show
 import fiona as f
 import numpy as np
 
+
+
 def generate_handles(labels, colors, edge='k', alpha=1):
     lc = len(colors)
     handles = []
@@ -16,29 +18,34 @@ def generate_handles(labels, colors, edge='k', alpha=1):
         handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
     return handles
 
-plt.ion()
+def scale_bar(ax, location=(0.92, 0.95)):
+    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
+    sbllx = (llx1 + llx0) / 2
+    sblly = lly0 + (lly1 - lly0) * location[1]
+
+    tmc = ccrs.TransverseMercator(sbllx, sblly)
+    x0, x1, y0, y1 = ax.get_extent(tmc)
+    sbx = x0 + (x1 - x0) * location[0]
+    sby = y0 + (y1 - y0) * location[1]
+
+    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=9, transform=tmc)
+    plt.plot([sbx, sbx - 5000], [sby, sby], color='k', linewidth=6, transform=tmc)
+    plt.plot([sbx-5000, sbx - 10000], [sby, sby], color='w', linewidth=6, transform=tmc)
+
 
 seadef = gpd.read_file('Data/seadef_sel.shp')
 dtm = gpd.read_file('Data/Raster_tr35_dt3.shp')
-myFig = plt.figure(figsize=(10, 10))
-
 myCRS = ccrs.UTM(29)
+myFig, ax = plt.subplots(1, 1, figsize=(12,12), subplot_kw=dict(projection=myCRS))
+
+
 
 ax = plt.axes(projection=ccrs.Mercator())
 
-print(seadef.head())
-seadef.crs
-dtm.crs
-seadef_bng = seadef.to_crs(epsg=27700)
-print(seadef_bng.head())
-dtm_bng = dtm.to_crs(epsg=27700)
-print(seadef_bng)
-print(seadef.crs == dtm.crs)
 
-join = gpd.sjoin(seadef, dtm, how='inner', lsuffix='left', rsuffix='right')
-join
 
-print(join)
+
+
 
 
 
